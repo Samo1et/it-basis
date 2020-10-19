422e74521d1a3453598c8b739674f9d1671bf1d6
abc <abc@Redmi.local>
2020-10-19 19:09:54 +0700

    add gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+import Copyright from '../components/Copyright';
+import Layout from '../components/Layout'
+
+
+export default function Index({location}) {
+  console.log(location)
+  return (
+    <Layout pathname={location.pathname}>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Gatsby v4-beta example 123
+          </Typography>
+          <Link to="/about" color="secondary">
+            Go to the about page
+          </Link>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </Layout>
+  );
+}
