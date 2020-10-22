e5f26d5d4e4d92d2c12c5c96a25e9968b232f554
abc <abc@Redmi.local>
2020-10-22 11:28:45 +0700

    remove dark them

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,22 @@
 import { red,purple } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { RedeemSharp } from '@material-ui/icons';
+
 
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
-    type: 'dark',
     primary: {
-      main: red[500],
+      main: purple[500],
     },
     secondary: {
       main: '#19857b',
     },
     error: {
       main: red.A400,
-    }
+    },
+    background: {
+      default: '#fff',
+    },
   },
 });
 
